25a95dbb06e2defcca59e21279c81e7f3c1f7ea5
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 14:12:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  # return array[0] if array.size == 1
+  #
+  # array.each do |word|
+  #   if array.index(word) != -1
+  #     array[array.index(word)] = word + ", "
+  #   else
+  #     array[array.index(word)] = "and " + word
+  #   end
+  # end
+  # array.join
+
+  return array[0] if array.size == 1
+  return array.join(" and ") if array.size == 2
+
+  array[-1] = "and " + array[-1]
+  return array.join(", ")
+
 
 end
